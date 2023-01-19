d81ff623371f4f3b078f38ec3083da62835b381b
ramsurat0880 <surajkumar231501@gmail.com>
2023-01-19 20:23:21 +0530

    day-3 tasks and issues completed

---
+++ quickart-app/src/Components/Applicences.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Applicences = () => {
+  return (
+    <div>Applicences</div>
+  )
+}
+
+export default Applicences
---
+++ quickart-app/src/Components/Topoffers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Topoffers = () => {
+  return (
+    <div>Topoffers</div>
+  )
+}
+
+export default Topoffers
